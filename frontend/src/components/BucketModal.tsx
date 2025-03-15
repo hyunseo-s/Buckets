797fbc6920e3dbeec7cf77c7f6f2beda57ea401f
Edison Chang <z5358446@ad.unsw.edu.au>
2025-03-16 05:54:58 +1100

    added bucket

--- frontend/src/components/BucketModal.tsx
+++ frontend/src/components/BucketModal.tsx
@@ -1,12 +1,8 @@
 import { Button, Modal, Select, TextInput } from "@mantine/core";
 import { useEffect, useState } from "react";
-import { handleError } from "../utils/handlers";
-import { get } from "../utils/apiClient";
-
-// export interface Group {
-// 	// id: string;
-// 	// buckets: string[];
-// }
+import { handleError, handleSuccess } from "../utils/handlers";
+import { get, post } from "../utils/apiClient";
+import { Group } from "../types";
 
 export const BucketModal = ({
   openedAddBucket,
@@ -19,7 +15,7 @@ export const BucketModal = ({
   const [bucketName, setBucketName] = useState<string>('');
   const [selectedBucketGroupOption, setBucketSelectedGroupOption] = useState<string | null>(null);
 
-  const [userGroups, setUserGroups] = useState([])
+  const [userGroups, setUserGroups] = useState<Group[]>([])
   
   useEffect(() => {
     const generateUserGroups = async () => {
@@ -28,20 +24,38 @@ export const BucketModal = ({
         handleError(res.error);
         return;
       }
-      console.log(res, "here")
-      // setUserGroups(res);
+      setUserGroups(res);
     }
 
     generateUserGroups();
   }, []);
 
+  const groupNameToId = (groupName: string) => {
+		const foundUsers = userGroups.filter(group => group.groupName === groupName);
+		if (foundUsers.length == 0) return null;
+		return foundUsers[0].groupId ?? null;
+	}
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const groupId = groupNameToId(selectedBucketGroupOption ?? "")
+
+    const params = {
+      bucketName: bucketName,
+      groupId: groupId
+    }
+    const res = await post("/buckets", params);
+    if (res.error) {
+      handleError(res.error);
+      return;
+    }
+    handleSuccess(res.message ?? "Success!");
+    closeAddBucket();
+  };	
+
   return (
     <Modal opened={openedAddBucket} onClose={closeAddBucket} title="Add Bucket" centered>
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        console.log('Bucket Name:', bucketName, 'Selected Group:', selectedBucketGroupOption);
-        closeAddBucket();
-      }}>
+      <form onSubmit={handleSubmit}>
         <TextInput
           label="Bucket Name"
           placeholder="Enter bucket name"
@@ -52,7 +66,7 @@ export const BucketModal = ({
         <Select
           label="Add to group"
           placeholder="Select group"
-          data={['Group 1', 'Group 2', 'Group 3']}
+          data={userGroups.map(group => group.groupName)}
           value={selectedBucketGroupOption}
           onChange={(value) => setBucketSelectedGroupOption(value)}
           required
