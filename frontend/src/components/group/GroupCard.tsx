6597440557d2d7540fa484f5eac2c79f89391e50
felixcao <felixcao37@gmail.com>
2025-03-15 18:30:14 +1100

    started group cards

--- frontend/src/components/group/GroupCard.tsx
+++ frontend/src/components/group/GroupCard.tsx
@@ -1,5 +1,18 @@
+import { Card, Image } from '@mantine/core';
 export const GroupCard = () => {
-	return (
-		<>Card</>
-	)
+  return (
+    <Card shadow="sm" padding="lg" radius="md" withBorder>
+      <Card.Section>
+        <Image
+          src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+          height={160}
+          alt="Norway"
+        />
+      </Card.Section>
+      {/* <Text size="sm" c="dimmed">
+        With Fjord Tours you can explore more of the magical fjord landscapes with tours and
+        activities on and around the fjords of Norway
+      </Text> */}
+    </Card>
+  );
 }
--- frontend/src/pages/Group.tsx
+++ frontend/src/pages/Group.tsx
@@ -1,11 +1,16 @@
 import { SearchBar } from '../components';
+import { GroupCard } from '../components/group/GroupCard';
 const Group = () => {
 	
+	const groups = [0];
 
   return (
 		<div style={{ height: "80vh", display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
 			<div style={{ width: "40%", minWidth: "300px", margin: "auto" }}>
 				< SearchBar />
+				<div>
+					{groups.map(group => <GroupCard />)}
+				</div>
 			</div>
 		</div>
   );
